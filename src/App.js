c9fa12901307cc00b24f97dfda97d3ac2ec7b737
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-07 15:58:48 +0200

    Add Shopping list Features

--- src/App.js
+++ src/App.js
@@ -1,21 +1,112 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Alert} from 'react-native';
+import {uuid} from 'uuidv4';
 
-class App extends React.Component {
-	render() {
-		return (
-			<View>
-				<Text style={styles.text}>Hello, world!</Text>
-			</View>
-		)
-	}
-}
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      text: 'Milk',
+    },
+    {
+      id: uuid(),
+      text: 'Eggs',
+    },
+    {
+      id: uuid(),
+      text: 'Bread',
+    },
+    {
+      id: uuid(),
+      text: 'Juice',
+    },
+  ]);
+
+  // Flag true if user is currently editing an item
+  const [editStatus, editStatusChange] = useState(false);
+
+  // State to capture information about the item being edited
+  const [editItemDetail, editItemDetailChange] = useState({
+    id: null,
+    text: null,
+  });
+
+  const [checkedItems, checkedItemChange] = useState([]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
+
+  // Submit the users edits to the overall items state
+  const saveEditItem = (id, text) => {
+    setItems(prevItems => {
+      return prevItems.map(item =>
+        item.id === editItemDetail.id ? {id, text: editItemDetail.text} : item,
+      );
+    });
+    // Flip edit status back to false
+    editStatusChange(!editStatus);
+  };
+
+  // Event handler to capture users text input as they edit an item
+  const handleEditChange = text => {
+    editItemDetailChange({id: editItemDetail.id, text});
+  };
+
+   // capture old items ID and text when user clicks edit
+  const editItem = (id, text) => {
+    editItemDetailChange({
+      id,
+      text,
+    });
+    return editStatusChange(!editStatus);
+  };
+
+  const itemChecked = (id, text) => {
+    const isChecked = checkedItems.filter(checkedItem => checkedItem.id === id);
+    isChecked.length
+      ? // remove item from checked items state (uncheck)
+        checkedItemChange(prevItems => {
+          return [...prevItems.filter(item => item.id !== id)];
+        })
+      : // Add item to checked items state
+        checkedItemChange(prevItems => {
+          return [...prevItems.filter(item => item.id !== id), {id, text}];
+        });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem
+            item={item}
+            deleteItem={deleteItem}
+            editItem={editItem}
+            isEditing={editStatus}
+            editItemDetail={editItemDetail}
+            saveEditItem={saveEditItem}
+            handleEditChange={handleEditChange}
+            itemChecked={itemChecked}
+            checkedItems={checkedItems}
+          />
+        )}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	text: {
-		fontWeight: "bold",
-		fontSize: 30
-	}
-})
+  container: {
+    flex: 1,
+  },
+});
 
-export default App
+export default App;
